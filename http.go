763f3b27d99560d524aebf61b4c4bd7a867c72fc
Roman <86300574+simbullar@users.noreply.github.com>
2022-08-06 20:16:53 +0300

    Update http.go

--- http.go
+++ http.go
@@ -7,7 +7,7 @@ import (
 )
 
 func main() {
-	resp, err := http.Get("https://api.telegram.org/bot5407477830:AAHRzUQ1_Yt-TQmhgYfqrTfEe3HWgXMNSJM/sendMessage?chat_id=856546075\&parse_mode=HTML&text=hello") //https://api.telegram.org/bot5407477830:AAHRzUQ1_Yt-TQmhgYfqrTfEe3HWgXMNSJM/sendMessage?chat_id=856546075\\&parse_mode=HTML&text=hello
+	resp, err := http.Get("https://api.telegram.org/bot5407477830:AAHRzUQ1_Yt-TQmhgYfqrTfEe3HWgXMNSJM/sendMessage?chat_id=856546075\\&parse_mode=HTML&text=hello") //https://api.telegram.org/bot5407477830:AAHRzUQ1_Yt-TQmhgYfqrTfEe3HWgXMNSJM/sendMessage?chat_id=856546075\\&parse_mode=HTML&text=hello
 	if err != nil {
 		log.Fatalln(err)
 	}
